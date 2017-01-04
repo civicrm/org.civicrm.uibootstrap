c6cca0efcbb1190d371160b493f560eb97bafd80
Tim Otten <totten@civicrm.org>
2017-01-03 22:47:33 -0800

    Initial import

---
+++ uibootstrap.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Implements hook_civicrm_angularModules().
+ *
+ * Generate a list of Angular modules.
+ *
+ * Note: This hook only runs in CiviCRM 4.5+. It may
+ * use features only available in v4.6+.
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_angularModules
+ */
+function uibootstrap_civicrm_angularModules(&$angularModules) {
+  $angularModules['ui.bootstrap'] = array(
+    'ext' => 'org.civicrm.uibootstrap',
+    'js' => array(
+      'node_modules/angular-ui-bootstrap/ui-bootstrap-tpls.min.js',
+    ),
+  );
+}
